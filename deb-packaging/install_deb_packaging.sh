f72ae48f8ecdbdd7e3fead1a158e7397c194452c
Benjamin Toll <benjam72@yahoo.com>
2023-07-09 13:50:04 -0400

    Add gnupg on installing or importing key can fail
    The .gnupg could have the wrong permissions and then the key won't be
    imported.

--- deb-packaging/install_deb_packaging.sh
+++ deb-packaging/install_deb_packaging.sh
@@ -9,6 +9,7 @@ apt-get install -y \
     dh-golang \
     dh-make \
     golang-any \
+    gnupg \
     openssh-server
 
 #locale-gen en_US.UTF-8
@@ -17,6 +18,8 @@ apt-get install -y \
 echo "StreamLocalBindUnlink yes" >> /etc/ssh/sshd_config
 /etc/init.d/ssh start
 
+# If the .gnupg dir doesn't have the correct permissions, the key can't be imported.
+# Should be either 0600 or 0700.
 curl -LO https://github.com/btoll/gpg-agent-forwarding/raw/master/public.key
 gpg --import public.key
 
