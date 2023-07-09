1632bcb56046f23bb227694d8d8867d7b4249102
Benjamin Toll <benjam72@yahoo.com>
2023-07-08 22:11:09 -0400

    Simplified build_deb.sh script

--- deb-packaging/build_deb.sh
+++ deb-packaging/build_deb.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-# shellcheck source=/dev/null
 
 set -euxo pipefail
 
-DEB="${PACKAGE_NAME}_${PACKAGE_VERSION}_amd64.deb"
-BUILDDIR="/root/build/${PACKAGE_NAME}/${PACKAGE_VERSION}/${PACKAGE_NAME}-${PACKAGE_VERSION}"
+trap cleanup EXIT
 
-if [ ! -d "$BUILDDIR" ]
-then
-    # `git-clone` will create the nested directory structure.
-    git clone "https://github.com/btoll/$PACKAGE_NAME.git" "$BUILDDIR"
-fi
+cleanup() {
+    rm -rf /build
+}
 
-cd "$BUILDDIR"
+DEB_BIN="${PACKAGE_NAME}_${PACKAGE_VERSION}_amd64.deb"
+DEB_SRC="${PACKAGE_NAME}_${PACKAGE_VERSION}.dsc"
+TARBALL="${PACKAGE_NAME}_${PACKAGE_VERSION}.tar.gz"
+
+BUILDDIRROOT="/root/build/${PACKAGE_NAME}/${PACKAGE_VERSION}"
+BUILDDIR="$BUILDDIRROOT/${PACKAGE_NAME}-${PACKAGE_VERSION}"
+
+# We'll clone into this directory, but the packages will be
+# placed in its parent (/build) went built.
+WORKDIR="/build/${PACKAGE_NAME}-${PACKAGE_VERSION}"
+
+# `git-clone` will create the nested directory structure.
+git clone "https://github.com/btoll/$PACKAGE_NAME.git" "$WORKDIR"
+
+cd "$WORKDIR"
 
 # The following will get the long id from the list of secret keys.
 # Specifically, the `sed` command will parse this:
@@ -31,12 +41,26 @@ KEYID=$(gpg \
 # This signs the `.dsc` and `.changes` files of the source package.
 dpkg-buildpackage \
 	--build=source,any,all \
+    --check-builddeps \
 	--force-sign \
+    --post-clean \
 	--root-command=fakeroot \
 	--sign-key="$KEYID"
 
 # Sign and let end-user decide if they want to verify using `debsig-verify` the `_deborigin`
 # file that `debsigs` puts in the `.deb` package.
-debsigs --sign=origin -k "$KEYID" "../$DEB"
+debsigs --sign=origin -k "$KEYID" "../$DEB_BIN"
+
+# Don't keep these package build assets:
+# - asbits-dbgsym_1.0.0_amd64.deb
+# - asbits_1.0.0_amd64.buildinfo
+# - asbits_1.0.0_amd64.changes
+
+# Since `systemd` is watching the build dir, don't create it until the last possible moment.
+mkdir -p "$BUILDDIR"
+mv "/build/$DEB_BIN" "$BUILDDIR"
+mv "/build/$DEB_SRC" "$BUILDDIR"
+mv "/build/$TARBALL" "$BUILDDIR"
+
 chown -R "$USER":"$USER" /root/build
 
